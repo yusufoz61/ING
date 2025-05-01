52e4e443041933082c78c1619b0c6b3aa79789c1
Yusuf Ozturk <yusuf.ozturk3@hva.nl>
2025-05-01 20:24:00 +0200

    say hello for everyone and for specific animals fixed

--- src/main/java/com/yusuf/zoo/Zoo.java
+++ src/main/java/com/yusuf/zoo/Zoo.java
@@ -7,11 +7,6 @@ import java.util.Scanner;
 public class Zoo {
     public static void main(String[] args)
     {
-        String[] commands = new String[4];
-        commands[0] = "hello";
-        commands[1] = "give leaves";
-        commands[2] = "give meat";
-        commands[3] = "perform trick";
 
         Lion henk = new Lion("henk");
         Hippo elsa = new Hippo("elsa");
@@ -19,19 +14,46 @@ public class Zoo {
         Tiger wally = new Tiger("wally");
         Zebra marty = new Zebra("marty");
         Monkey chimp = new Monkey("chimp");
-        Elephant Harrison = new Elephant("Harrison");
+        Elephant harrison = new Elephant("harrison");
+
+        Animal[] animals = {
+                henk,
+                elsa,
+                dora,
+                wally,
+                marty,
+                chimp,
+                harrison
+        };
+
+
+        String[] commands = new String[4];
+        commands[0] = "hello";
+        commands[1] = "give leaves";
+        commands[2] = "give meat";
+        commands[3] = "perform trick";
+
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Voer uw command in: ");
-
         String input = scanner.nextLine();
-        if(input.equals(commands[0] + " henk"))
+
+        String[] words = input.split(" ");
+
+        if(input.equals(commands[0]))
         {
-            henk.sayHello();
+            for(Animal animal : animals){
+                animal.sayHello();
+            }
+        }else if(words.length >= 2){
+            String name = words[1];
+            for(Animal animal : animals){
+                if(animal.getName().equals(name)){
+                    animal.sayHello();
+                }
+            }
+
         }
-        else
-        {
-            System.out.println("Unknown command: " + input);
-        }
+
     }
 }
